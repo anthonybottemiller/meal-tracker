bf0b6a2686d35e602c3b362581d25c09a8e6d592
Anthony Bottemiller <anthonybottemiller@outlook.com>
2017-01-20 12:38:34 -0800

    add editFood Method and edit-food tag to template

--- app/app.component.ts
+++ app/app.component.ts
@@ -5,7 +5,8 @@ import { Food } from './food.model';
   selector: 'app-root',
   template: `
   <h1>Meal Tracker</h1>
-  <food-list [childFoodList]="masterFoodList"></food-list>
+  <food-list [childFoodList]="masterFoodList" (clickSender)="editFood($event)"></food-list>
+  <edit-food [childSelectedFood]="selectedFood"></edit-food>
   <new-food (newFoodSender)="addFood($event)"></new-food>
   `
 })
@@ -15,8 +16,13 @@ export class AppComponent {
     new Food(100, 'Success fries', 'It works so far have some potatoe!'),
     new Food(1000, 'Success Whopper', 'It is a good day')
   ];
+  selectedFood = null;
 
   addFood(newFoodFromChild: Food) {
     this.masterFoodList.push(newFoodFromChild);
+  }
+
+  editFood(clickedFood) {
+    this.selectedFood = clickedFood;
   }
 }
