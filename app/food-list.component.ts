0daff9ccab0e63075c586eb503e34e0a0577e33d
Anthony Bottemiller <anthonybottemiller@outlook.com>
2017-01-20 13:27:19 -0800

    add filter criteria selection to food-list component

--- app/food-list.component.ts
+++ app/food-list.component.ts
@@ -4,8 +4,14 @@ import { Food } from './food.model';
 @Component({
   selector: 'food-list',
   template: `
+    <label>What calorie range do you wish to view?</label>
+    <select (change)="onFilterChange($event.target.value)">
+      <option value="all">All Items</option>
+      <option value="highCalorie">High Calories</option>
+      <option value="lowCalorie">Low Calories</option>
+    </select>
     <ul>
-      <li *ngFor="let currentFood of childFoodList">"{{currentFood.name}}" Calories: {{currentFood.calories}}
+      <li *ngFor="let currentFood of childFoodList | calories:filterByCalories">"{{currentFood.name}}" Calories: {{currentFood.calories}}
       <button (click)="editClicked(currentFood)">Edit!</button>
       </li>
     </ul>
@@ -15,8 +21,14 @@ export class FoodListComponent {
   @Input() childFoodList: Food[];
   @Output() clickSender = new EventEmitter();
 
+  filterByCalories: string = "all";
+
   editClicked(foodToEdit: Food) {
     this.clickSender.emit(foodToEdit);
+  }
+
+  onFilterChange(optionFromSelect) {
+    this.filterByCalories = optionFromSelect;
   }
 
 }
