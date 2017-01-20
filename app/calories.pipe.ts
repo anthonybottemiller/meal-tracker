2dce8be0838b1e2fdf47db6b0c4347e4cea33fd4
Anthony Bottemiller <anthonybottemiller@outlook.com>
2017-01-20 13:26:32 -0800

    add pipe logic

--- app/calories.pipe.ts
+++ app/calories.pipe.ts
@@ -10,6 +10,24 @@ export class CaloriesPipe implements PipeTransform {
 
   transform(input: Food[], calorieOption) {
     var output: Food[] = [];
-    
+    if (calorieOption === "highCalorie") {
+      for (var i = 0; i < input.length; i++) {
+        if (input[i].calories >= 500) {
+          output.push(input[i]);
+        }
+      }
+      return output;
+    }
+    else if (calorieOption === "lowCalorie") {
+      for (var i = 0; i < input.length; i++) {
+        if (input[i].calories < 500) {
+          output.push(input[i]);
+        }
+      }
+      return output;
+    }
+    else {
+      return input;
+    }
   }
 }
