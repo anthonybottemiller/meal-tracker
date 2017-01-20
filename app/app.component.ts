d08ff404808be68025bec253faf51faa9c6ce250
Anthony Bottemiller <anthonybottemiller@outlook.com>
2017-01-20 11:16:07 -0800

    add code to initialize a test food and hand it to the food-list component

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,12 +1,17 @@
 import { Component } from '@angular/core';
+import { Food } from './food.model';
 
 @Component({
   selector: 'app-root',
   template: `
   <h1>Meal Tracker</h1>
+  <food-list [childFoodList]="masterFoodList"></food-list>
   `
 })
 
 export class AppComponent {
+masterFoodList: Food[] = [
+  new Food(100, 'Success fries', 'It works so far have some potatoe!')
+];
 
 }
